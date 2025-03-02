9e08114ef46115e2066d25e3cda14d285edf1004
jhj <121736308@qq.com>
2025-03-02 22:36:28 +0800

    创建后端

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,19 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+    @RequestMapping("getbotinfo/")
+    public Map<String,String> getBotInfo(){
+        Map<String,String> map = new HashMap<>();
+        map.put("name","lf");
+        map.put("version","1.0.0");
+        return map;
+    }
+
+}
