7d8611455abd5d7b049af129be067ec4788da6e5
jhj <121736308@qq.com>
2025-03-08 00:44:00 +0800

    配置Msql与注册登录模块

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,21 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+
+
     @Test
     void contextLoads() {
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodePassword = passwordEncoder.encode("pjhj");
+        System.out.println(encodePassword);
+
     }
 
 }
