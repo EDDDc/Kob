ecf45b062a3a5270c34800e88cb0d8616e834454
haijun jiang <121736308@qq.com>
2025-05-26 11:35:16 +0800

    完成登录界面 实现退出

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,15 +1,62 @@
 <template>
   <NavBar/>
-  <router-view/>
+  <router-view></router-view>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+// import $ from 'jquery'
 export default {
   components: {
     NavBar
+  },
+  setup() {
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/token/",
+    //   type: "post",
+    //   data: {
+    //     username: "bb",
+    //     password: "pbb",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/info/",
+    //   type: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkOTQyMTdlOTA4ZjE0NThlOGM5ODQ3NDBmYzFhYzU2NSIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTc0Nzk3MDkxOCwiZXhwIjoxNzQ5MTgwNTE4fQ.9hlRuuUn2pD4GZV_tlKL1hISEwE083Ah2mgkWA708eg"
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "jhj2",
+    //     password: "pjhj",
+    //     confirmPassword: "pjhj",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
   }
   
 }
