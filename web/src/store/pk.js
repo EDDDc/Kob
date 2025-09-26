d57095fb01db5d10e2b98c787e27ff3d5191d2d6
haijun jiang <121736308@qq.com>
2025-09-26 21:06:49 +0800

    跟至6.2 1:35:00

--- web/src/store/pk.js
+++ web/src/store/pk.js
@@ -14,7 +14,7 @@ export default {
         b_sx: 0,
         b_sy: 0,
         gameObject: null,
-
+        loser: "none",
   },
   getters: {
   },
@@ -40,6 +40,9 @@ export default {
     },
     updateGameObject(state, gameObject) {
         state.gameObject = gameObject;
+    },
+    updateLoser(state, loser) {
+        state.loser = loser;
     }
   },
   actions: {
