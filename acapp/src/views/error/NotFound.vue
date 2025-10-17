2db1ba9b1a9db6581c715bccb1b470f2cc46b480
haijun jiang <121736308@qq.com>
2025-10-17 20:03:28 +0800

    10.1 37:10

---
+++ acapp/src/views/error/NotFound.vue
@@ -0,0 +1,17 @@
+v<template>
+    <ContentField>404 Not Found</ContentField>
+</template>
+
+<script>
+import ContentField from "@/components/ContentField.vue"
+
+export default {
+    components: {
+        ContentField
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
